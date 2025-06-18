1fb216bd910daa39963e79149543674d45b51394
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-06-18 18:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-st.title("Welcome to Car Price Prediction App")
-st.markdown("""
-This app helps you **predict the price of a used car** based on various features like make, Engine , year, mileage, and more.
-
-Use the sidebar to navigate between:
-- **Home** - You're here!
-- **Info** - Learn about the data and features used.
-- **Predict** - Enter car details and get the estimated price.
-""")
