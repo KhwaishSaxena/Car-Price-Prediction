39e2b3d69c9191d61b7d9c425772067fefe4d316
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-06-18 18:47:35 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import xgboost as xgb
 import os
 #model = pickle.load(open("xgboost_model.pkl", "rb"))
 # Get absolute path of current directory (pages/)
