47c2d57127f6f6c1d42fde706570c39482b056cd
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-06-19 09:43:53 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -7,22 +7,24 @@ import os
 model = pickle.load(open("xgboost_model.pkl", "rb"))
 st.title('Car Price Prediction')
 
-make = st.selectbox("Make", ['Honda', 'Maruti Suzuki', 'Hyundai', 'Toyota', 'Mercedes-Benz', 'BMW', 'Skoda', 'Nissan', 'Renault', 'Tata', 'Volkswagen', 'Ford', 'Audi', 'Mahindra', 'MG', 'Jeep', 'Porsche', 'Kia', 'Land Rover', 'Volvo', 'Maserati', 'Jaguar', 'Isuzu', 'Fiat', 'MINI', 'Ferrari', 'Mitsubishi', 'Datsun', 'Lamborghini', 'Chevrolet', 'Ssangyong', 'Rolls-Royce', 'Lexus'], help = "Company Of the car")
+make_mapping = {k: i for i, k in enumerate(['Honda', 'Maruti Suzuki', 'Hyundai', 'Toyota', 'Mercedes-Benz', 'BMW', 'Skoda', 'Nissan', 'Renault', 'Tata', 'Volkswagen', 'Ford', 'Audi', 'Mahindra', 'MG', 'Jeep', 'Porsche', 'Kia', 'Land Rover', 'Volvo', 'Maserati', 'Jaguar', 'Isuzu', 'Fiat', 'MINI', 'Ferrari', 'Mitsubishi', 'Datsun', 'Lamborghini', 'Chevrolet', 'Ssangyong', 'Rolls-Royce', 'Lexus'])}
+fuel_mapping = {k: i for i, k in enumerate(["Petrol", "Diesel", "CNG", "Others", "Electric"])}
+owner_mapping = {k: i for i, k in enumerate(['First', 'Second', '3 or More', 'UnRegistered Car'])}
+drive_mapping = {k: i for i, k in enumerate(['FWD', 'RWD', 'AWD'])}
+trans_mapping = {k: i for i, k in enumerate(['Manual', 'Automatic'])}
+seller_mapping = {k: i for i, k in enumerate(['Individual', 'Corporate', 'Commercial Registration'])}
+color_mapping = {k: i for i, k in enumerate(['White', 'Silver', 'Blue', 'Black', 'Grey', 'Red', 'Others'])}
+location_mapping = {k: i for i, k in enumerate(['Mumbai', 'Delhi', 'Bangalore', 'Hyderabad', 'Chandigarh', 'Lucknow', 'Kolkata', 'Ahmedabad', 'Patna', 'Chennai', 'Jaipur', 'Others'])}
 
-fuel_type = st.selectbox("Fuel Type", ["Petrol", "Diesel", "CNG", "Others", "Electric"], help = "Fuel type of the car")
-
-owner = st.selectbox("Owner", ['First','Second','3 or More','UnRegistered Car'], help = "Number of previous owners")
-
-drivetrain = st.selectbox("Drivetrain", ['FWD','RWD','AWD'], help = "AWD/RWD/FWD" )
-
-transmission = st.selectbox("Transmission", ['Manual','Automatic'], help = "Gear transmission of the car")
-
-seller_type = st.selectbox("Seller Type ", ['Individual','Corporate','Commercial Registration'], help = "tells if car is sold by individual or dealer")
-
-color = st.selectbox("Color", ['White','Silver','Blue','Black','Grey','Red','Others'], help = "Color of the car")
-
-Loc = st.selectbox("Location", ['Mumbai','Delhi','Bangalore','Hyderabad','Chandigarh','Lucknow','Kolkata','Ahmedabad','Patna','Chennai','Jaipur','Others' ], help = "City in which car is being sold")
-
+# Apply the encodings
+make = make_mapping[make]
+fuel_type = fuel_mapping[fuel_type]
+owner = owner_mapping[owner]
+drivetrain = drive_mapping[drivetrain]
+transmission = trans_mapping[transmission]
+seller_type = seller_mapping[seller_type]
+color = color_mapping[color]
+Loc = location_mapping[Loc]
 year = st.number_input("Year", min_value = 1990, format = "%d", help = "Manufacturing Year of the car")
 km = st.number_input("Kilometers", min_value = 0, format = "%d", help = "Total kilometers Driven")
 engine = st.number_input("Engine", min_value = 0, format = "%d", help = "engine capacity of the car in cc")
