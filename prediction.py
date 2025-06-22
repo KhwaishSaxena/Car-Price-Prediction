d6468744457640954dc0d7052f7475d247735b48
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-06-22 16:59:30 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -7,8 +7,6 @@ model = pickle.load(open("xgboost_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
 encoder = pickle.load(open("encoder.pkl", "rb"))
-st.write(encoder)
-st.write(encoder['make'].classes_)
 
 st.title("Car Price Prediction")
 
